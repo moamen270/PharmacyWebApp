9541a130657e03bfb564168d27db21e6b9eaae38
Moamen Mostafa <mmoamen270@gmail.com>
2022-09-19 21:11:52 +0200

    ..

--- PharmacyWebApp.DataAccess/ApplicationDbContext.cs
+++ PharmacyWebApp.DataAccess/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using PharmacyWebApp.Models;
 
 namespace PharmacyWebApp.DataAccess
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
+    
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
