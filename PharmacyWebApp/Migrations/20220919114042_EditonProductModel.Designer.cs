72398a698115dbcc693e4543927a9676dbf5cb62
Mohamed Hazem <mh600766@gmail.com>
2022-09-19 13:58:02 +0200

    Edit On Product

--- PharmacyWebApp/Migrations/20220919114042_EditonProductModel.Designer.cs
+++ PharmacyWebApp/Migrations/20220919114042_EditonProductModel.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PharmacyWebApp.DataAccess;
 
@@ -10,9 +11,10 @@ using PharmacyWebApp.DataAccess;
 namespace PharmacyWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220919114042_EditonProductModel")]
+    partial class EditonProductModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
