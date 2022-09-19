a62080af2e4db6c9705bfe73484bb69d1b3b877d
Moamen Mostafa <mmoamen270@gmail.com>
2022-09-19 13:29:01 +0200

    Add Online Database

---
+++ PharmacyWebApp/Models/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace PharmacyWebApp.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PharmacyWebApp/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace PharmacyWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
