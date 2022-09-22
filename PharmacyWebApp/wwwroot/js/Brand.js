ce221958434787c897baf62d23c3c5c0d3203494
Moamen Mostafa <mmoamen270@gmail.com>
2022-09-22 19:33:50 +0200

    Add Brand Controller and View

--- PharmacyWebApp/wwwroot/js/Brand.js
+++ PharmacyWebApp/wwwroot/js/Brand.js
@@ -24,5 +24,33 @@ function Delete(url) {
             })
         }
     });
+}
+
+function Create(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'POST',
+                success: function (data) {
+                    if (data.success) {
+                        location.reload();
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    });
 
 }
