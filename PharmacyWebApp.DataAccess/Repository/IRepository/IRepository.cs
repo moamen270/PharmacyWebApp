6457ccf67027f252824f1e8a89b73ab7895f85c7
Moamen Mostafa <mmoamen270@gmail.com>
2022-09-24 18:31:12 +0200

    Async Brand

--- PharmacyWebApp.DataAccess/Repository/IRepository/IRepository.cs
+++ PharmacyWebApp.DataAccess/Repository/IRepository/IRepository.cs
@@ -10,16 +10,22 @@ namespace PharmacyWebApp.DataAccess.Repository.IRepository
     public interface IRepository<T> where T : class
     {
         T Get(int id);
+        Task<T> GetAsyce(int id);
         IEnumerable<T> GetAll();
+        Task<IEnumerable<T>> GetAllAsync();
         T GetFirstOrDefault(Expression<Func<T,bool>>? filter=null);
+        Task<T> GetFirstOrDefaultAsync(Expression<Func<T, bool>>? filter=null);
         T Add(T entity);
+        Task<T> AddAsync(T entity);
         IEnumerable<T> AddRange(IEnumerable<T> entities);
-        T Update(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        T Update(T entity);      
         void Delete(T entity);
         void DeleteRange(IEnumerable<T> entities);
         int Count();
         int Count(Expression<Func<T, bool>> filter);
-
+        Task<int> CountAsync();
+        Task<int> CountAsync(Expression<Func<T, bool>> filter);
 
 
     }
